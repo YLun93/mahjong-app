3524d05f21274ef3446dff956ca3eeae8d5e3002
YLun93 <yenlun0518@icloud.com>
2026-01-08 02:12:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = 'mahjong-v1';
-const ASSETS = [
+const CACHE_NAME = 'mahjong-cache-v1';
+const ASSETS_TO_CACHE = [
   './',
-  './index.html', // 假設你的主檔案是 index.html
+  './index.html',
+  './manifest.json',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
   'https://cdn.tailwindcss.com'
 ];
 
-// 安裝並快取資源
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-// 攔截請求，優先從快取抓取
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
